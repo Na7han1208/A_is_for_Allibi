360413e71494832df37027f39037ff7c45b9e2d8
Na7han1208 <125017312+Na7han1208@users.noreply.github.com>
2025-09-15 11:07:07 +0200

    + TutorialHelper.cs

---
+++ Detect/Assets/Scripts/TutorialHelper.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class TutorialHelper : MonoBehaviour
+{
+    [Header("Have they done it?")]
+    private bool moved = false;
+    private bool looked = false;
+    private bool pickedUp = false;
+    private bool crouched = false;
+    private bool inspected = false;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
